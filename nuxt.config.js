d83303b4231d0dfccd31301cce3efe572854ed6a
hstrejolunaoasis <htrejo@oasishoteles.com>
2023-03-05 16:57:38 -0500

    added bootstrap

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,10 +21,12 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~/node_modules/bootstrap/dist/css/bootstrap.min.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/bootstrap.js', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,9 @@
 <template>
-  <h1>Hello hstrejoluna</h1>
+    <h1>Hello hstrejoluna</h1>
 </template>
 
 <script>
 export default {
-  mounted() {
-    console.log(this.$content)
-
-  }
-}; 
+  layout: 'default'
+};
 </script>
