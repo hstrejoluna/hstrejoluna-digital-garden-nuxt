9aebd60f4cd794c88e78c33ac923775b4d396f73
hstrejoluna <trejolunatutoriales@gmail.com>
2023-05-10 21:42:16 -0500

    page: poetry and thoughts category created

---
+++ pages/poetry/index.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+  async asyncData({ $content }) {
+    const notes = await $content("notes")
+      .where({ category: { $eq: 'poetry' } })
+      .fetch();
+
+    return {
+      notes
+    }
+  },
+  data: () => ({
+    selectedTag: ""
+  }),
+  computed: {
+    displayedNotes() {
+      if (this.selectedTag) {
+        return this.notes.filter(note => note.tags.includes(this.selectedTag))
+      } else {
+        return this.notes
+      }
+    }
+  }
+}
+</script>
+<template>
+  <section class="container pt-5">
+    <h1>My Notes About Poetry and Thoughts</h1>
+    <p v-show="selectedTag">Filtered by: {{ selectedTag }}
+      <button @click="selectedTag = ''">Clear</button>
+    </p>
+    <ul>
+      <li v-for="note in displayedNotes" :key="note.slug + note.createdAt">
+        <h2>
+          <nuxt-link :to="`/notes/${note.slug}`">
+            {{ note.title }}
+
+          </nuxt-link>
+        </h2>
+        <ul>
+          <li v-for="tag in note.tags" :key="note.slug + tag" @click="selectedTag = tag">
+            {{ tag }}</li>
+        </ul>
+      </li>
+    </ul>
+  </section>
+</template>
